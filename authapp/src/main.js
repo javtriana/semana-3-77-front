ef8dd6151a04d71fd1f0bffdc6ed6abce7e5b1eb
javtriana <44759635+javtriana@users.noreply.github.com>
2020-12-16 17:26:18 -0500

    Add files via upload

---
+++ authapp/src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+// import store from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+
+const base = axios.create({
+  baseURL: "http://localhost:3000"
+});
+
+Vue.prototype.$http = base;
+
+Vue.config.productionTip = false;
+new Vue({
+  router,
+  // store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ authapp/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <home />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import home from "../components/Home.vue";
+export default {
+  name: "Home",
+  components: {
+    home
+  }
+};
+</script>
